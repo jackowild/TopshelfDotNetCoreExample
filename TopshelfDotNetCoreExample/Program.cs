b5360926fd8a0e80524e70d0541e36e01edb73d2
Jack Wild <jackwild123@gmail.com>
2019-04-22 17:48:49 +0100

    Add project files.

---
+++ TopshelfDotNetCoreExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using Serilog;
+using System.Timers;
+using Topshelf;
+
+namespace TopshelfDotNetCoreExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(windowsService =>
+            {
+                windowsService.Service<ServiceExample>(s =>
+                {
+                    s.ConstructUsing(service => new ServiceExample());
+                    s.WhenStarted(service => service.Start());
+                    s.WhenStopped(service => service.Stop());
+                });
+
+                windowsService.RunAsLocalSystem();
+                windowsService.StartAutomatically();
+
+                windowsService.SetDescription("TopshelfDotNetCoreExample");
+                windowsService.SetDisplayName("TopshelfDotNetCoreExample");
+                windowsService.SetServiceName("TopshelfDotNetCoreExample");
+            });
+        }
+    }
+
+    class ServiceExample
+    {
+        private Timer timer;
+        private int count;
+
+        public ServiceExample()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("C:\\LogFiles\\TopshelfDotNetCoreExample\\log.txt")
+                .CreateLogger();
+
+            this.timer = new Timer() { AutoReset = true, Interval = 30000 };
+            this.timer.Elapsed += (s, e) => Log.Information($"Count = {this.count++}");
+        }
+
+        public void Start()
+        {
+            Log.Information("Started");
+            this.timer.Start();
+        }
+
+        public void Stop()
+        {
+            Log.Information("Stopped");
+            this.timer.Stop();
+        }
+    }
+}
